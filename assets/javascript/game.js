5282769f0c3b6e5e14d77137baf546cdcd74ebcf
DobienC <dobien_c@yahoo.com>
2019-01-05 23:03:54 -0800

    functionality issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,35 +4,96 @@ var loses = 0;
 var guesses = [];
 var holder = [];
 var ansVal = "a";
-var possible ="abcdefghijklmnopqrstuvwxyz"
+var possible = ["red","blue","green","yellow","pink"]
+var word;
+
+function inWord(letter, index){
+    if(letter === word.charAt(index)){
+        return true;
+    }
+    return false;
+}
+
+function inList(letter){
+    for(var i = 0; i < holder.length; i++){
+        if(holder[i] !== "-"){
+            return true;
+        }
+    }
+    return false;
+}
+
+function isComplete(){
+    for(var i = 0; i < word.length; i++){
+        if(holder[i] === "-"){
+            return false;
+        }
+    }
+    return true;
+}
+
+function changeWord(){
+    word = possible[Math.floor(Math.random() * 5)];
+}
+
+function resetHolder(){
+    holder = [];
+    for(var j = 0; j < word.length; j++){
+        holder.push("-");
+    }
+}
 
 $(document).keypress(function(event){   //65-90
     if(guesses_left === 10){
-        ansVal = possible.charAt(Math.floor(Math.random() * possible.length));
+        changeWord(); //possible.charAt(Math.floor(Math.random() * possible.length));
+        resetHolder();
     }
-    var charVal = String.fromCharCode(event.keyCode);
-    for(var i = 0; i < guesses.length; i++){
-        holder.push(guesses[i]);
-    }
-    holder.push(charVal);
-    guesses = holder;
-    holder = [];
     guesses_left--;
-    if(ansVal === charVal){
-        alert("You guessed it! The answer was: " + ansVal);
+    holder = guesses;
+    guesses = [];
+    for(var i = 0; i < word.length; i++){
+        ansVal = String.fromCharCode(event.keyCode);
+        if(inWord(ansVal,i)){
+            guesses.push(ansVal); 
+        }else if(inList(ansVal)){
+            guesses.push(holder[i]);
+        } else {
+            guesses.push("-");
+        }
+    }
+    // if(guesses_left === 10){
+    //     ansVal = possible; //possible.charAt(Math.floor(Math.random() * possible.length));
+    // }
+    // var charVal = String.fromCharCode(event.keyCode);
+    // for(var i = 0; i < guesses.length; i++){
+    //     holder.push(guesses[i]);
+    // }
+    // holder.push(charVal);
+    // guesses = holder;
+    // holder = [];
+    // guesses_left--;
+    // if(ansVal === charVal){
+    //     alert("You guessed it! The answer was: " + ansVal);
+    //     wins++;
+    //     guesses_left = 10;
+    //     guesses = [];
+    // }
+    // if(guesses_left === 0){
+    //     loses++;
+    //     guesses_left = 10;
+    //     guesses = [];
+    // }
+    if(isComplete()){
         wins++;
         guesses_left = 10;
-        guesses = [];
-    }
-    if(guesses_left === 0){
-        loses++;
+    } else if(guesses_left === 0){
         guesses_left = 10;
-        guesses = [];
+        loses++;
     }
     $('#wins').html(wins); //event.keyCode
     $('#loses').html(loses);
     $('#guesses_left').html(guesses_left);
-    $('#guesses').html(guesses);
+    $('#guesses').text(guesses);
 });
 
 // $(document).ready(function(){
